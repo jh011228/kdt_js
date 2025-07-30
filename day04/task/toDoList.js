66255058f3102246b23d46d56f508e09ab01a9d2
jh011228 <jh01122844@gmail.com>
2025-07-30 19:12:37 +0900

    250730 19:12 task 수정

--- day04/task/toDoList.js
+++ day04/task/toDoList.js
@@ -3,21 +3,30 @@ const taskList = document.getElementById("task-list");
 
 button.addEventListener("click", function () {
   let ment = prompt('추가할 할일을 입력하세요');
-  if (ment.length > 0) {
-    let newLi = document.createElement('li');
-    // 태그 추가
+  let newLi = document.createElement('li');
+  // 태그 추가
     newLi.setAttribute('class', 'list-li');
-    newLi.innerHTML = "<div class='bottom-line'><input type='checkbox' id='list-checkbox' onclick='bgGreen()'> " + ment +
-      "<button type='button' class='btn-delete'>삭제</button></div>";
-    taskList.appendChild(newLi);
-  }
+    newLi.innerHTML = "<input type='checkbox' id='list-checkbox'> " + ment +
+    "<button type='button' class='btn-delete'>삭제</button>";
+    if(ment!=null && ment.length!=0){
+      taskList.appendChild(newLi);
+    }
+  
+  
+  let delBtn = newLi.querySelector(".btn-delete");
+  delBtn.addEventListener('click',function(){
+    if(confirm('할일을 삭제하시겠습니까?') ){
+      this.parentElement.remove();
+    } 
+  });
+
+  let check = newLi.querySelector("#list-checkbox");
+  check.addEventListener('click',function(e){
+    if(e.target.checked){
+      this.parentElement.style.backgroundColor='green';
+    }
+    else{
+      this.parentElement.style.backgroundColor='white';
+    }
+  });
 });
-
-
-
-function bgGreen(){
-  let myDiv = document.querySelector('.list-checkbox');
-  let parent = myDiv.parentElement;
-  let parent2 = parent.parentElement;
-  parent2.style.backgroundColor = 'green';
-}
